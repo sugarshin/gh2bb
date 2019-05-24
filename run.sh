f0a9a067b8179e0bedb722920136a180768c5de4
Shingo Sato <shinsugar@gmail.com>
2019-05-24 10:27:48 +0900

    Change env name + fix bb api

--- run.sh
+++ run.sh
@@ -11,15 +11,15 @@ REPOS=$(curl "https://api.github.com/$GITHUB_TARGET_TYPE/$GITHUB_TARGET_USERNAME
 
 for repo in ${REPOS[@]}; do
   echo "> $repo"
-  [ -d $repo ] || git clone git@github.com:$GITHUB_TARGET_USERNAME/$repo.git $repo
+  [ -d $repo ] || git clone git@github.com:${GITHUB_TARGET_USERNAME}/${repo}.git $repo
   cd $repo
   git fetch origin --prune
   git reset --hard origin/HEAD
   curl -XPOST https://bitbucket.org/api/2.0/repositories/${BITBUCKET_USERNAME}/${GITHUB_TARGET_USERNAME}_${repo} \
     --fail \
-    --user $BITBUCKET_USERNAME:$BITBUCKET_PASSWORD \
+    --user ${BITBUCKET_USERNAME}:${BITBUCKET_APP_PASSWORD} \
     -H 'Content-Type: application/json' \
     -d '{"is_private":true}' || true
-  git remote add bb git@bitbucket.org:$BITBUCKET_USERNAME/$repo.git || true
+  git remote add bb git@bitbucket.org:${BITBUCKET_USERNAME}/${GITHUB_TARGET_USERNAME}_${repo}.git || true
   git push bb --force
 done
